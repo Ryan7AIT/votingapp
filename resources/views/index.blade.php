efd6766245fbc0069c4afadb86c2467e490e8167
Ryan7AIT <aitmechedalryan7@gmail.com>
2022-02-14 17:53:19 +0100

    desing the ideas page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,52 @@
 <x-app-layout>
 
-	
+	<div class="ideas-contaier p-3 space-x-8">
+		<div class="idea-container bg-white rounded-xl py-3 px-3 text-gray-900 flex ">
+
+			<div class=" px-5 py-8 border-r border-gray-900 ">
+		        <div class="text-center">
+		            <div class="font-semibold text-2xl ">11</div>
+		            <div class="text-gray-500">Votes</div>
+		        </div>
+
+		        <div class="mt-8">
+		            <button class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 hover:border-gray-400 text-xxs rounded-xl">Vote</button> </div>
+	        </div>
+
+	        <div class="mx-8 flex flex-col justify-between">
+	        	<div class="space-y-4">
+		        	<h5 class="font-bold text-xl">first idea title</h5>
+		        	<p class="font-semibold">Ryan</p>
+										        		
+	        	</div>
+
+	        	<div class="mb-4">
+	        			<div class="space-y-4">
+	        				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil fuga sapiente aspernatur tempora ipsa recusandae? Sunt ut harum incidunt inventore mollitia nisi iure earum fuga at sit repellat, quidem quisquam.</p>
+
+	        			<div class="flex justify-between mb-4">
+	        				<div class="flex justify-arround space-x-14">
+	        					<div>Catgeory 1</div>
+	        					<div>Catgeory 1</div>
+	        					<div>Catgeory 1</div>
+	        				</div>
+	        				
+
+	        				<div class="ml-5">
+	        					<svg class="h-8 w-8 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="1" />  <circle cx="19" cy="12" r="1" />  <circle cx="5" cy="12" r="1" /></svg>
+	        				</div>
+
+	        			</div>
+	        	</div>
+	        	</div>
+
+
+	        	
+
+	        </div>
+
+    	</div>
+
+	</div>
 
 </x-app-layout>
